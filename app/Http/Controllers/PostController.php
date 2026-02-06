67d2c49943c5f8dcde0aeb86097fd271a8147c68
sajib <sajibg7@gmail.com>
2026-02-07 01:34:43 +0600

    feat(create migration):add posts table and model

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+{
+    $posts = Post::latest()->paginate(20);
+    return view('posts.index', compact('posts'));
+}
 }
